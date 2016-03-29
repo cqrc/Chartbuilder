580f29325fb0b2a3a1b5af80b787aabfc98016fa
Henry Chan <henry.chan.tii@gmail.com>
2016-03-29 14:50:01 -0400

    removed validation func from index

--- src/js/index.js
+++ src/js/index.js
@@ -10,21 +10,6 @@ var ChartbuilderLocalStorageAPI = require("./util/ChartbuilderLocalStorageAPI");
 var Chartbuilder = require("./components/Chartbuilder.jsx");
 var container = document.querySelector(".chartbuilder-container");
 
-var _validateMeta = function(metadata) {
-	var errorMessArr = [];
-
-	if ( !(typeof metadata.title === 'string' && metadata.title.length > 0) ) {
-		errorMessArr.push({
-							location : "",
-							text : "The title field is empty",
-							type : "error"
-						});
-
-	}
-
-	return errorMessArr;
-};
-
 document.addEventListener("DOMContentLoaded", function() {
 	document.cookie = "authed=yes";
 	// Initialize data from localStorage
@@ -34,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		<Chartbuilder
 			showMobilePreview={false}
 			enableJSONExport={true}
-			validateMeta={_validateMeta}
 		/>,
 	container );
 
